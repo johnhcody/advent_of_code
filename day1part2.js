5ade45c9208cc7f2b0ac04a4f396c31ff650098b
johnhcody <john.haner.cody@gmail.com>
2020-12-03 11:45:03 +0700

    three sum solution

--- day1part2.js
+++ day1part2.js
@@ -6,18 +6,21 @@ const threeSum = async () => {
     const numbers = lines.map(Number);
     let hashObj = {}
 
-    for (let i = 0; i < numbers.length; i++) {
-        let thisNum = numbers[i];
-        hashObj[thisNum] = i;
+    for (let i = 0; i < numbers.length - 2; i++) {
+        for (let j = 1; j < numbers.length - 1; j++) {
+            if (j > 1) {
+                hashObj[numbers[i] + numbers[j]] = [numbers[i], numbers[j]];
+            }
+        }
     }
 
     for (let i = 0; i < numbers.length; i++) {
         let difference = 2020 - numbers[i];
         if (hashObj.hasOwnProperty(difference)) {
-            return difference * numbers[i];
+            twoSum = hashObj[difference];
+            return numbers[i] * twoSum[0] * twoSum[1];
         }
     }
-
     return 1;
 }
 
